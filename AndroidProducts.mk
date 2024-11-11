519e8bc119d4e5305c52891ad28e642c10a220fa
Xiburse <50038191+Xiburse@users.noreply.github.com>
2024-11-11 18:18:48 +0800

    Update AndroidProducts.mk, add COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_gts9pwifi.mk
+COMMON_LUNCH_CHOICES := \
+    lineage_gts9pwifi-eng \
+    lineage_gts9pwifi-userdebug \
+    lineage_gts9pwifi-user
